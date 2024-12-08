5a6028d802ce4f08c57760e0658fe0e1d2da62b1
Ahmad Allahverdiyev <dvlpr37@gmail.com>
2024-12-08 09:33:06 +0400

    added filters

--- shop/products/views.py
+++ shop/products/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.shortcuts import get_object_or_404
 
 from rest_framework import status
@@ -18,6 +19,30 @@ class ProductAPIView(APIView):
         else:
             products = Product.objects.all()
             paginator = PageNumberPagination()
+
+            filters = Q()
+            name = request.query_params.get('name')
+            is_vip = request.query_params.get('is_vip')
+            min_price = request.query_params.get('min_price')
+            max_price = request.query_params.get('max_price')
+            min_rating = request.query_params.get('min_rating')
+            max_rating = request.query_params.get('max_rating')
+
+            if name:
+                name = ' '.join(name.split('-'))
+                filters &= Q(name__icontains=name)
+            if is_vip:
+                filters = Q(is_vip=is_vip.capitalize())
+            if min_price:
+                filters &= Q(price_usd__gte=min_price)
+            if max_price:
+                filters &= Q(price_usd__lte=max_price)
+            if min_rating:
+                filters &= Q(rating__gte=min_rating)
+            if max_rating:
+                filters &= Q(rating__lte=max_rating)
+
+            products = products.filter(filters).order_by('rating')
             paginated_products = paginator.paginate_queryset(products, request)
             serializer = ProductSerializer(paginated_products, many=True)
             return Response(serializer.data, status=status.HTTP_200_OK)
